061197fdc2c8f4b1e05614e2538b48622d39d6bf
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-10-01 13:23:52 +0300

    create task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,27 @@
 const btnStartRef = document.querySelector('[data-start]');
 const btnStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+const PROMT_DELAY = 1000;
+let intervalId = null;
+
+btnStartRef.addEventListener('click', onClickBtnStart);
+btnStopRef.addEventListener('click', onClickBtnStop);
+
+btnStopRef.disabled = true;
+
+function onClickBtnStart(evt) {
+  evt.target.disabled = true;
+  btnStopRef.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, PROMT_DELAY);
+}
+
+function onClickBtnStop(evt) {
+  evt.target.disabled = true;
+  btnStartRef.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
